025ea59360b60007ca74188af6ca5aedbf44035e
Sean2525 <madness48596@gmail.com>
2018-11-11 19:29:54 +0800

    Updates

---
+++ precache-manifest.adb39d2e61f0c0e67cdc2e3b3094c0d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "53da9c10226e26a72f8d",
+    "url": "/VAWSR/static/css/main.138319ba.chunk.css"
+  },
+  {
+    "revision": "53da9c10226e26a72f8d",
+    "url": "/VAWSR/static/js/main.53da9c10.chunk.js"
+  },
+  {
+    "revision": "4f7634d45345e626b802",
+    "url": "/VAWSR/static/css/1.cf46eff9.chunk.css"
+  },
+  {
+    "revision": "4f7634d45345e626b802",
+    "url": "/VAWSR/static/js/1.4f7634d4.chunk.js"
+  },
+  {
+    "revision": "16d56a444d489ab9b0ec",
+    "url": "/VAWSR/static/js/runtime~main.16d56a44.js"
+  },
+  {
+    "revision": "fe9e7d6aad574cfd19e2fe99a00bb535",
+    "url": "/VAWSR/index.html"
+  }
+];
